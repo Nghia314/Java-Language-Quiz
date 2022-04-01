24aa4a34864a67a8e17cebf26a678f1fc79f344b
Nghia Le <davidle314@hotmail.com>
2022-04-01 07:03:11 -0700

    redo Js to make it more easy understand for me

--- script.js
+++ script.js
@@ -1,60 +1,38 @@
 //create question and choices
 var questions = [
-    { title:"What is the extension of java code files?",
+    { Number: 1,
+      question:"What is the extension of java code files?",
       choices:(".js","txt",".class",".java"),
       Answer: ".java"
     },
-    { title:"What is printed (system.out.print(hello,\nworld!);?",
+    {Number: 2,
+       title:"what is printed (system.out.print(hello,\nworld!);?",
       choices:("Hello,\nworld!","Hello,world!","Nothing","None of the above"),
       Answer: "Nothing" 
 
     },
-    { title: "Which of the following variable declaration would NOT compile in a java program?",
+    { Number: 3,
+      title: "Which of the following variable declaration would NOT compile in a java program?",
       choices:("int var;","int VAR;","int var1;","int1_var;"),
       Answer: "Int1_var;" 
 
     },
-    { title:"What is the size of boolean variable?",
+    { Number: 4,
+      title:"What is the size of boolean variable?",
       choices:("8 bit","16 bit","32 bit","not precisely defined"),
       Answer: "16 bit" 
 
     },
-    { title:"which of the following is true about String",
+    {Number: 5,
+       title:"which of the following is true about String",
       choices:("String is mutable","String is immutable","string is a date type","None of the above"),
       Answer: "String is immutable" 
 
     },
-    { title:"What is a correct syntax to output (Hello world) in java?",
+    {Number: 6,
+       title:"What is a correct syntax to output (Hello world) in java?",
       choices:("system.out.print('hello world')","echo('hello world')","print('Hello world')"),
       Answer: "system.out.print('hello world')" 
 
     },
 ];
-//score varible
-var score = 0;
-
-//API
-var time = document.querySelector(".current-Time");
-var container = document.querySelector(".container");
-var question = document.querySelector(".question");
-var timer = document.querySelector(".start")
-var ul = document.querySelector(".choices")
-
-//timer varibles
-// 10s per question
-var secondsleft = 60
-// hold interval time
-var holdInterval = 0;
-//penalty time
-var penalty = 5;
-
-timer.addEventListener("click", function() {
-    if (holdInterval === 0) {
-        holdInterval = setInterval(function() {
-            secondsleft--;
-            time.textContent = "time: " + secondsleft;
-
-            if ()
-        }
-    }
-}
