0c0aceb39f6926f9c6a66bdb41c234bb611c0b1c
Nghia Le <davidle314@hotmail.com>
2022-04-03 19:03:12 -0700

    refactor the code to make the page function '

--- script.js
+++ script.js
@@ -1,95 +1,89 @@
-// Dom Object
-var QestionContaienr = document.getElementById("QestionContaienr");
-var question = document.getElementById("question");
-var Answer = document.getElementById("Answer");
-var Timerplace = document.getElementById("Timerplace");
-var scoreplace = document.getElementById("scoreplace");
-var startButton = document.getElementById("startButton");
-var showscore = document.getElementById("showscore");
-//varibles
-let Timer = 48;
-let runningTimer;
-let score = 0;
-let qNumber ;
-let finalScore;
-let Max_Score = 6
+var startButton = document.getElementById("start-btn");
+var questionContainerElement = document.getElementById("questions-container");
+var questionElement = document.getElementById("question");
+var answerElementButton = document.getElementById("answer-buttons");
 
-//funtion to start game and see highscore
-startButton.addEventListener("click",startGame);
-showscore.addEventListener("click", displayScores);
+let shuffledQuestion, currentQuestionIndex;
 
-function startGame() {
+startButton.addEventListener("click", startgame);
+
+function startgame() {
+  console.log("started");
   startButton.classList.add("hide");
-  scoreArea.classList.add("hide");
-  AnswerButton.classList.remove("hide");
-  qNumber=0;
-  scoreplace.innerHTML= "";
-  startClock();
-  while (AnswerButton.firstChild);{
-    AnswerButton.removeChild(AnswerButton.firstChild);
-  }
-  showQuestion(question[qNumber]);
+  shuffledQuestion = questions.sort(() => Math.random() - 0.5);
+  currentQuestionIndex = 0;
+  questionContainerElement.classList.remove("hide")
+  setNextQuestion()
 }
-//display question after hit start
-function showQuestion(question) {
-  question.innerHTML = question.question;
-  question.answers.forEach(answer => {
-    var button = document.createElement("button");
-    button.innerHTML.add("btn");
-    if (answer.Answer) {
-      button.dataset.Answer = answer.Answer;
-    }
-    button.addEventListener("click", SelectAnswer);
-    answersButton.appendChild(button);
-  });
+
+function setNextQuestion() {
+  showQuestion(shuffledQuestion[currentQuestionIndex]);
 }
+function showQuestion(questions) {
+  questionElement.innerText = questions.questions
+  questions.Answer.forEach(Answer =>{
+    const button = document.createElement("button")
+    button.innerText = asnwer.Choice
+    button.classList.add("btn")
+  })
+}
+
+function selectAnswer() {}
+
 //create question and choices,asnwer
-let questions = [
-   { 
-     question:"What is the extension of java code files?",
-     ChoiceA:".js",
-     ChoiceB:"txt",
-     ChoiceC:".class",
-     ChoiceD:".java",
-     Answer: D
-   },
-   {
-    question:"What is printed (system.out.print(hello,\nworld!);?",
-    ChoiceA:"hello,\nworld",
-    ChoiceB:"hello,world",
-    ChoiceC:"Hello world",
-    ChoiceD:"None of above",
-    Answer: D
+var questions = [
+  {
+    question: "What is the extension of java code files?",
+    Answer: [
+      { Choice: ".js", correct: false },
+      { Choice: "txt", correct: false },
+      { Choice: ".class", correct: false },
+      { Choice: ".java", correct: true },
+    ],
   },
   {
-    question:"Which of the following variable declaration would Not compile in a java program?",
-    ChoiceA:"int var;",
-    ChoiceB:"int VAR;",
-    ChoiceC:"int var1",
-    ChoiceD:"int1_var;",
-    Answer: D
+    question: "What is printed (system.out.print(hello,\nworld!);?",
+    Answer: [
+      { Choice: "hello,\nworld", correct: false },
+      { Choice: "hello,world", correct: false },
+      { Choice: "Hello world", correct: false },
+      { Choice: "None of above", correct: true },
+    ],
   },
   {
-    questions:"What is the size of boolean variable?",
-    ChoiceA:"8 bit",
-    ChoiceB:"16 bit",
-    ChoiceC:" 32 bit",
-    ChoiceD:"not precisely defined",
-    Answer: B
+    question:
+      "Which of the following variable declaration would Not compile in a java program?",
+    Answer: [
+      { Choice: "int var;", correct: false },
+      { Choice: "int VAR;", correct: false },
+      { Choice: "int var1", correct: false },
+      { Choice: "int1_var;", correct: true },
+    ],
   },
   {
-    questions:"Which of the following is true about String?",
-    ChoiceA:"String is mutable",
-    ChoiceB:"String is immutable",
-    ChoiceC:"String is a data type",
-    ChoiceD:"None of the above",
-    Answer: B
+    questions: "What is the size of boolean variable?",
+    Answer: [
+      { Choice: "8 bit", correct: false },
+      { Choice: "16 bit", correct: true },
+      { Choice: " 32 bit", correct: false },
+      { Choice: "not precisely defined", correct: false },
+    ],
   },
   {
-    questions:"What is a correct syntaxx to output (Hello world) in java?",
-    ChoiceA:"System.out.print('hello world')",
-    ChoiceB:"echo('hello world')",
-    ChoiceC:"print('Hello World')",
-    Answer: A
+    questions: "Which of the following is true about String?",
+    Answer: [
+      { Choice: "String is mutable", correct: false },
+      { Choice: "String is immutable", correct: true },
+      { Choice: "String is a data type", correct: false },
+      { Choice: "None of the above", correct: false },
+    ],
+  },
+  {
+    questions: "What is a correct syntaxx to output (Hello world) in java?",
+    Answer: [
+      { Choice: "System.out.print('hello world')", correct: true },
+      { Choice: "echo('hello world')", correct: false },
+      { Choice: "print('Hello World')", correct: false },
+    ],
   },
 ];
