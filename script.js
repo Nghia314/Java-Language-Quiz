85abb5f7afb98bac389ecdc13f853fabdc989273
Nghia Le <davidle314@hotmail.com>
2022-04-05 15:27:37 -0700

    at create a submit button

--- script.js
+++ script.js
@@ -3,50 +3,33 @@
 var questions = [
   {
     question: "What is the extension of java code files?",
-      text: ".js",
-      text: "txt",
-      text: ".class",
-      text: ".java",
+      text: [".js","txt",".class",".java"],
       answer: ".java"
   },
   {
     question: "What is printed (system.out.print(hello,\nworld!);?",
-      text: "hello,\nworld",
-      text: "hello,world",
-      text: "Hello world",
-      text: "None of above",
+      text: ["hello,\nworld", "hello,world","Hello world","None of above"],
       answer: "None of above"
   },
   {
     question:
       "Which of the following variable declaration would Not compile in a java program?",
-      text: "int var;",
-      text: "int VAR;",
-      text: "int var1",
-      text: "int1_var;",
+      text: ["int var;", "int VAR;","int var1","int1_var;"],
       answer: "int1_var;"
   },
   {
     question: "What is the size of boolean variable?",
-      text: "8 bit",
-      text: "16 bit",
-      text: " 32 bit",
-      text: "not precisely defined",
+      text: ["8 bit","16 bit"," 32 bit","not precisely defined"],
       answer: "16 bit"
   },
   {
     question: "Which of the following is true about String?",
-      text: "String is mutable",
-      text: "String is immutable",
-      text: "String is a data type",
-      text: "None of the above",
+      text: ["String is mutable","String is immutable","String is a data type","None of the above"],
       answer: "String is immutable"
   },
   {
     question: "What is a correct syntaxx to output (Hello world) in java?",
-      text: "System.out.print('hello world')",
-      text: "echo('hello world')",
-      text: "System.println('hello world')",
+      text: ["System.out.print('hello world')","echo('hello world')", "System.println('hello world')"],
       answer: "System.out.print('hello world')"
   },
 ];
@@ -94,9 +77,9 @@ function render(currentQuestionindex) {
     var choices = questions[currentQuestionindex].text;
     questionContainerElement.textContent = numquestions;
   }
-  choices.forEach(function(newitem) {
+  choices.forEach(function(item) {
     var listchoice = document.createElement("li")
-    listchoice.textContent = newitem;
+    listchoice.textContent = item;
     questionContainerElement.appendChild(ulCreate);
     ulCreate.appendChild(listchoice);
     listchoice.addEventListener("click", (compare));
@@ -119,10 +102,55 @@ function compare(event) {
   currentQuestionindex++;
 
   if( currentQuestionindex >= questions.length) {
-    gameOver();
+    GameOver();
     createDiv.textContent = "Quiz end!" + "" + "You got " + point + "/" + questions.length + "Correct!";
   } else {
     render(currentQuestionindex);
   }
   questionContainerElement.appendChild(createDiv);
 }
+
+function GameOver() {
+  questionContainerElement.innerText="Game Over";
+  timerRemaining.innerText="";
+
+  
+  var createh1= document.createElement("h1");
+  createh1.setAttribute("id", "createh1");
+  createh1.textContent ="Finish!"
+
+  createh1.appendChild(createh1)
+
+  var createp= document.createElement("p");
+  createp.setAttribute("id", "p");
+
+
+  createp.appendChild(createp)
+  
+  if(timeleft >= 0) {
+    var timerRemaining= timeleft;
+    var createp2 = document.createElement("p");
+    clearInterval(secondInterval);
+    createp.textContent="final score: "+ timerRemaining;
+
+    questionContainerElement.appendChild(createp2)
+  }
+} 
+
+var label = document.createElement("label")
+label.setAttribute("type", "text");
+label.textContent = "Your Initials: ";
+
+questionContainerElement.appendChild(label);
+
+
+var input = document.createElement("input");
+input.setAttribute("type", "text");
+input.setAttribute("id", "Initials");
+input.textContent = "";
+
+questionContainerElement.appendChild(input);
+
+var submit = document.createElement("button");
+submit.setAttribute("id", "Submit");
+submit.setAttribute("type", "Submit");
